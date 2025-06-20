fa80a2b776269b1a49dbdce05603fc77360a76a2
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-06-20 15:43:23 +0200

    fixed protoc

--- docker/install-basic-deps.sh
+++ docker/install-basic-deps.sh
@@ -16,6 +16,8 @@ ARCH=$(uname -m)
 
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 
+cd /usr
+
 # x86_64
 if [ "$ARCH" = "x86_64" ]; then
   curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
@@ -27,5 +29,4 @@ else
 fi
 
 unzip *.zip
-cp bin/protoc /usr/bin
 chmod 755 /usr/bin/protoc
