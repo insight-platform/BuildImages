bf1503bed3d8abb0ae6660af9db10faa13137b13
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-06-20 15:36:30 +0200

    added missing script

---
+++ docker/install-basic-deps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+pip3 install maturin==1.8.7 patchelf cffi ziglang sccache>=0.4.0
+
+curl -o rustup.sh --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs
+sh rustup.sh -y
+source $HOME/.cargo/env
+rustup update
+rustc -V
+
+cargo install cargo-chef --locked
+
+ARCH=$(uname -m)
+
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+
+# x86_64
+if [ "$ARCH" = "x86_64" ]; then
+  curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
+elif [ "$ARCH" = "aarch64" ]; then
+  curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-aarch_64.zip
+else
+    echo "Unsupported architecture $ARCH"
+    exit 1
+fi
+
+unzip *.zip
+cp bin/protoc /usr/bin
+chmod 755 /usr/bin/protoc
