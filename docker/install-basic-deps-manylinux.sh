fb91f7c14e7efa44c79631624b0464b11f143aae
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-06-20 15:34:37 +0200

    fix

--- docker/install-basic-deps-manylinux.sh
+++ docker/install-basic-deps-manylinux.sh
@@ -5,4 +5,4 @@ set -e
 dnf install -y epel-release
 dnf install -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
 dnf install -y --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
-RUN dnf install -y ffmpeg ffmpeg-devel clang clang-devel openssl-devel jq
+dnf install -y ffmpeg ffmpeg-devel clang clang-devel openssl-devel jq
